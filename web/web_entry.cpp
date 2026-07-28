63a004538d5d1c35f0afa265a2a8a6ed5ab8a5dc
Ilya Tolstoukhov <88771362+myldy20@users.noreply.github.com>
2026-07-28 16:34:39 +0300

    Add browser entry point

---
+++ web/web_entry.cpp
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-FileCopyrightText: 2026 Myldy Design
+
+extern "C" int SDL_main(int argc, char** argv);
+
+int main(int argc, char** argv) {
+    return SDL_main(argc, argv);
+}
